a59b9a8b39519856369ef5721ed0633e290c788a
Sardar Noor Burhan <58101399+sardarnb@users.noreply.github.com>
2021-02-26 17:05:07 +0500

    Update arbart_dash.py

--- arbart_dash.py
+++ arbart_dash.py
@@ -1,6 +1,7 @@
 import dash
 import dash_table
 import pandas as pd
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_table as dt
@@ -17,7 +18,30 @@ app.layout = dbc.Container([
     html.H1('Arb finder - Bart'),
     html.H3('View Golf Matchups between BT and BM'),
     dbc.Button('Refresh', id='refresh-match-table'),
-    dt.DataTable(
+    dbc.Spinner(html.Div(id='match_table_show')),
+    dcc.Interval(
+        id='interval-component',
+        interval=60 * 1000,  # in milliseconds
+        n_intervals=0
+    )
+])
+
+
+@app.callback(
+    Output('match_table_show','children'),
+    [Input('refresh-match-table','n_clicks'),
+    Input('interval-component', 'n_intervals'),
+     ])
+def show_matches(x, y):
+    df_matchups = pd.read_csv('https://raw.githubusercontent.com/sardarnb/df_arbart/main/df_arbart.csv')
+    df = df_matchups[['match_time_bm', 'away_player', 'home_player', 'away_decimal_odds_bm', 'home_decimal_odds_bm',
+             'match_time_bto', 'away_decimal_odds_bto', 'home_decimal_odds_bto', 'best_away', 'best_home', 'cum_prob',
+             'log_time']]
+    df.log_time = pd.to_datetime(df.log_time)
+    # df.loc[df.log_time == df.log_time.max()]
+    data = df.tail(10).to_dict('records'),
+
+    m_table = dt.DataTable(
         id='match_table',
         columns=[{'name': 'match_time_bm', 'id': 'match_time_bm'},
                  {'name': 'away_player', 'id': 'away_player'},
@@ -31,11 +55,8 @@ app.layout = dbc.Container([
                  {'name': 'best_home', 'id': 'best_home'},
                  {'name': 'cum_prob', 'id': 'cum_prob'},
                  {'name': 'log_time', 'id': 'log_time'},
-                 # {'name': 'Operator', 'id': 'operator'},
-                 # {'name': 'Country', 'id': 'country'},
-                 # {'name': 'Well Name', 'id': 'well_nm'}
                  ],
-        # data=[],
+        data=data[0],
         # row_selectable='single',
         style_cell_conditional=[
             {
@@ -64,55 +85,9 @@ app.layout = dbc.Container([
         },
         filter_action="native",
     )
-])
 
-
-@app.callback(
-    Output('match_table','data'),
-    [Input('refresh-match-table','n_clicks'),
-     ])
-def show_matches(x):
-    df = pd.read_csv('https://raw.githubusercontent.com/sardarnb/df_arbart/main/df_arbart.csv')
-    df = df[['match_time_bm', 'away_player', 'home_player', 'away_decimal_odds_bm', 'home_decimal_odds_bm',
-             'match_time_bto', 'away_decimal_odds_bto', 'home_decimal_odds_bto', 'best_away', 'best_home', 'cum_prob',
-             'log_time']]
-    df.log_time = pd.to_datetime(df.log_time)
-    df.loc[df.log_time == df.log_time.max()]
-    data = df.tail(10).to_dict('records'),
-    #
-    # users = []
-    #
-    # for result in results:
-    #     users.append({
-    #         'id' : result[0],
-    #         'username' : result[1],
-    #         'email' : result[2],
-    #         'admin' : str(result[3]),
-    #         'operator': result[4],
-    #         'country': result[5],
-    #         'well_nm': result[6],
-    #
-    #     })
-
-    # match_table = dash_table.DataTable(
-    #     id='table',
-    #     columns=[{"name": i, "id": i} for i in df.columns],
-    #     data=show_matches(),
-    #     row_selectable='single',
-    #     style_data={
-    #         'whiteSpace': 'normal',
-    #         'height': 'auto',
-    #     },
-    #     style_cell={
-    #         'overflow': 'hidden',
-    #         'textOverflow': 'ellipsis',
-    #         'maxWidth': 0
-    #     },
-    #     filter_action="native",
-    # )
-
-    return data[0]
+    return m_table
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
